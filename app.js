5677f51eca5aeb53f97f6c031ad6bd6d5512de54
rober99.guedes@gmail.com <rober99.guedes@gmail.com>
2024-03-22 22:37:52 -0600

    arreglado conflicto

--- app.js
+++ app.js
@@ -4,19 +4,15 @@
 const questions = document.querySelectorAll(".question");
 const ques = document.querySelector(".question");
 
-console.log(questions)
+console.log(questions);
 questions.forEach((question) => {
-  const btn=question.querySelector(".question-btn");
-  btn.addEventListener("click",e=>{
-    questions.forEach(ite => {
-<<<<<<< HEAD
-        if(ite!==question)ite.classList.remove("show-text");
-=======
-        ite.classList.remove("show-text");
->>>>>>> 2a270e8748a2c61e00f9dbca5787ecaa70432ec0
+  const btn = question.querySelector(".question-btn");
+  btn.addEventListener("click", (e) => {
+    questions.forEach((ite) => {
+      if (ite !== question) ite.classList.remove("show-text");
     });
 
-    question.classList.toggle("show-text")
-    console.log(question)
-  })
+    question.classList.toggle("show-text");
+    console.log(question);
+  });
 });
