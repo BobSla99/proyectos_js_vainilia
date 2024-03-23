517399192f1f66ca9275d0acf280824f57614f8e
rober99.guedes@gmail.com <rober99.guedes@gmail.com>
2024-03-22 22:16:42 -0600

    proyecto 7 termindo con codigo del tuto

--- app.js
+++ app.js
@@ -1,30 +1,18 @@
 //using selectors inside the element
 // traversing the dom
 
-const btns = document.querySelectorAll(".question-btn");
+const questions = document.querySelectorAll(".question");
 const ques = document.querySelector(".question");
 
-btns.forEach((btn) => {
-  btn.addEventListener("click", (e) => {
-    //le ando a la clase al question selected
-    const question = e.currentTarget.parentElement.parentElement;
-    question.classList.toggle("show-text");
-
-
-    //selecciono todos los demas questions
-    const questionS = document.querySelectorAll(".question");
-    //los llevo a un array para poder manipularlos
-    arrayQuestionS = Array.from(questionS);
-    //recojo  todos loslque no son el presionado
-    arrayQuestionS = arrayQuestionS.filter(
-      (thisQuestion) => thisQuestion !== question
-    );
-    //y si estan visibles los ocultos
-    arrayQuestionS.forEach((ques) => {
-      ques.classList.contains("show-text")
-        ? ques.classList.remove("show-text")
-        : null;
+console.log(questions)
+questions.forEach((question) => {
+  const btn=question.querySelector(".question-btn");
+  btn.addEventListener("click",e=>{
+    questions.forEach(ite => {
+        if(ite!==question)ite.classList.remove("show-text");
     });
-    console.log(e.currentTarget.parentElement.parentElement);
-  });
+
+    question.classList.toggle("show-text")
+    console.log(question)
+  })
 });
